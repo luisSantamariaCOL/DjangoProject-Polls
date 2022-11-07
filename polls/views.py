56d36704f0f45e26b92d1929087b06ba08a95a30
luisSantamariaCol <luisestebansantamaria@outlook.com>
2022-11-07 13:37:44 -0500

    "superuser creation and views created"

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+# displays a question text, with no results but with a form to vote.
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+# displays results for a particular question.
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {}".format(question_id))
+
+
+
