fc15b144a4d1b9a312c812eb069b953b07d3fc01
luisSantamariaCol <luisestebansantamaria@outlook.com>
2022-11-08 09:22:44 -0500

    "create simple django template"

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(template.render(context, request))
 
 # displays a question text, with no results but with a form to vote.
 def detail(request, question_id):
